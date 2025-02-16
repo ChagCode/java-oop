a83fe4f1ce9c52e56285a761f171647a018aaf0c
chagcode <chag.open@gmail.com>
2025-02-16 21:29:49 +0300

    java - oop: multithreading v2

--- src/multithreading/Main.java
+++ src/multithreading/Main.java
@@ -3,43 +3,43 @@ package multithreading;
 import java.util.Random;
 
 public class Main {
-  //  static boolean isFive = false;
+    static boolean isFive = false;
   static boolean timeStop = false;
 
   public static void main(String[] args) {
     // создание дополнительного потока
-//    Thread thread = new Thread(new Runnable() {
-//      @Override
-//      public void run() {
-//        for (int i = 0; i < 10_000_000; i++) {
-//          if (i == 5) {
-//            isFive = true;
-//          }
-//          System.out.println(i);
-//          try {
-//            Thread.sleep(1000);
-//          } catch (InterruptedException _) {
-//          }
-//        }
-//      }
-//    });
+    Thread thread = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        for (int i = 0; i < 10_000_000; i++) {
+          if (i == 5) {
+            isFive = true;
+          }
+          System.out.println(i);
+          try {
+            Thread.sleep(1000);
+          } catch (InterruptedException _) {
+          }
+        }
+      }
+    });
 //    thread.start();
-//    Thread thread2 = new Thread(new Runnable() {
-//      @Override
-//      public void run() {
-//        for (int i = 0; i < 10_000_000; i++) {
-//          // остановим 2-ой доп.поток, когда равно 5
-//          if (isFive) {
-//            break;
-//          }
-//          System.out.println(i);
-//          try {
-//            Thread.sleep(1000);
-//          } catch (InterruptedException _) {
-//          }
-//        }
-//      }
-//    });
+    Thread thread2 = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        for (int i = 0; i < 10_000_000; i++) {
+          // остановим 2-ой доп.поток, когда равно 5
+          if (isFive) {
+            break;
+          }
+          System.out.println(i);
+          try {
+            Thread.sleep(1000);
+          } catch (InterruptedException _) {
+          }
+        }
+      }
+    });
 //    thread2.start();
     // основной (главный) поток
 //    for (int i = 0; i < 10_000_000; i++) {
